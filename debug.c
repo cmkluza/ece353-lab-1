a532c15a60993d826deb4dd3dcb16ad3fc13d384
Cameron Kluza <cmkluza@gmail.com>
2018-10-05 11:43:49 -0400

    fixed includes and macros

--- debug.c
+++ debug.c
@@ -1,8 +1,9 @@
 #include "debug.h"
+#include <stdarg.h>
+#include <stdio.h>
 
-#ifdef DEBUG // if DEBUG is defined at compile time, log debug statements
 void debug(const char *file, const char *function, int line,
-        const char *fmt, ...) {
+           const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
 
@@ -10,7 +11,3 @@ void debug(const char *file, const char *function, int line,
     vfprintf(stderr, fmt, args);
     fprintf(stderr, "\n");
 }
-#else // otherwise, do nothing
-void debug(const char *file, const char *function, int line,
-       const char *fmt, ...) {}
-#endif
--- debug.h
+++ debug.h
@@ -2,7 +2,7 @@
 #define DEBUG_H
 
 #include <stdlib.h>
-#include <varargs.h>
+#include <stdarg.h>
 
 /**
  * Logs a debugging statement.
@@ -11,10 +11,13 @@ void debug(const char *file, const char *function, int line,
         const char *fmt, ...);
 
 /*
- * Macro that makes using the debug statement easier.
+ * If DEBUG is defined when compiling (using -DDEBUG on gcc), will print debugging info
  */
+#ifdef DEBUG
 #define PRINT_DEBUG(fmt, ...) do { \
     debug(__FILE__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__); \
 } while (0)
-
+#else
+#define PRINT_DEBUG(fmt, ...)
+#endif
 #endif
