47ca12b4abdb254d54bbe49d0ecaabcfebf631e7
ldeburgo <43252101+ldeburgo@users.noreply.github.com>
2018-10-06 12:21:47 -0400

    Update cache.h

--- cache.h
+++ cache.h
@@ -91,14 +91,14 @@ int tagBits(Cache *cache, int addr);
  */
 int hitWay(Cache *cache, unsigned long addr){
     
-    if (whichSet(Cache *cache, int addr)==-1){    //if whichSet returns -1 it is a miss
-        updateOnMiss(Cache *cache, int addr);
+    if (whichSet(Cache *cache, unsigned long addr)==-1){    //if whichSet returns -1 it is a miss
+        updateOnMiss(Cache *cache, unsigned long addr);
         return -1;
     }
     
     else{                                         //else it is a hit
-        updateOnHit(Cache *cache, int addr);
-        return whichSet(Cache *cache, int addr)+1;    
+        updateOnHit(Cache *cache, unsigned long addr);
+        return whichSet(Cache *cache, unsigned long addr)+1;    
     }
 }
 /**
